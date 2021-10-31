73671366120b15a4149cb7d4aabd2328296394f0
李志钊 <lizhizhaooo@163.com>
2021-10-31 13:01:09 +0800

    init

---
+++ src/test/java/com/lzz/seleniumExample/SeleniumExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzz.seleniumExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
